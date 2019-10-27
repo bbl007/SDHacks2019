d7f3410a9fec56d6ebc8e5384d26e602cb108bbc
Alejandro Fosado <afosado@ucsd.edu>
2019-10-27 04:59:55 -0700

    api fixes

--- image/app.py
+++ image/app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 
 full_df = pd.read_csv('https://raw.githubusercontent.com/bbl007/SDHacks2019/master/image/full_df.csv')
-# val_ser = full_df.iloc[:, 7:].sum(axis=1)
+val_ser = full_df.iloc[:, 7:].sum(axis=1)
 ing_ser = full_df['ingredients'].apply(lambda x: x.count('\n')+1)
 
 @app.route('/')
@@ -27,16 +27,16 @@ def recommend():
     ingredients = items.get('ing', '')
     ingredients = ingredients.split(',')
     ingredients = [' '.join(x.split('_')) for x in ingredients]
-    n = items.get('n', 10)
+    n = int(items.get('n', 10))
     
     similarity = np.zeros(len(full_df))
     for ing in ingredients:
         similarity += full_df[ing]
         
-    no_extra = items.get('no_extra', 0)
+    no_extra = items.get('nextra', 0)
     if no_extra:
         has_ing = similarity > 0
-        similarity = similarity - (similarity - ing_ser)
+        similarity = 3*similarity - var_ser - ing_ser
         temp_full = full_df.loc[has_ing]
         food_sim = pd.DataFrame({'similarity':similarity}).loc[has_ing]
     else:
