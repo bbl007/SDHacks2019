12c4fc0fc0b2b9d305f3b5e857506a8ebc3a1db6
Kevin Lee <khl089@ucsd.edu>
2019-10-27 00:30:33 -0700

    updatedAPp

--- leftoversAndroid/Leftovers/app/src/main/java/com/example/leftovers/RecipeActivity.java
+++ leftoversAndroid/Leftovers/app/src/main/java/com/example/leftovers/RecipeActivity.java
@@ -1,29 +1,63 @@
 package com.example.leftovers;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class RecipeActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+    private RecipeAdapter adapter;
+
+    private Recipe chosen;
+
+    List<Recipe> recipeList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recipe);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        recipeList = new ArrayList<>();
+
+        recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        recipeList.add(new Recipe(
+           0, "BLT", 5
+        ));
+
+        recipeList.add(new Recipe(
+                1, "Tomato Beef", 3
+        ));
+
+        adapter = new RecipeAdapter(this, recipeList);
+
+        adapter.setOnItemClickListener(new RecipeAdapter.OnItemCLickListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onItemClick(int position) {
+                chosen = recipeList.get(position);
+
+                System.out.println("LAUNCH RESULT");
+                Intent launchResults = new Intent(RecipeActivity.this, ResultsActivity.class);
+                launchResults.putExtra("chosen", chosen.getIndex() + "");
+                startActivity(launchResults);
             }
         });
+
+        recyclerView.setAdapter(adapter);
     }
+
 
 }
